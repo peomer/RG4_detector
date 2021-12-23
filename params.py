5678ef54e1266da253c889b1131e8e04da9ca030
peomer <95092006+peomer@users.noreply.github.com>
2021-12-23 19:58:59 +0200

    Add files via upload

---
+++ params.py
@@ -0,0 +1,12 @@
+Cloud_run = True
+Data_run_list = ['seq','mfe','evo','plfold']
+Debug = False
+save_model = False
+Load_model = False
+ITER      = 1
+add_mfe = False
+AVG_TF_SEED_AMOUNT = 3
+Show_plots =False
+Save_plots = False
+Save_Results = True
+Use_Test = True
